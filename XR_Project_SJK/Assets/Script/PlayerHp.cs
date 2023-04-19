d6d52e68c6da6e4608ef81c302781e36d4d53852
gitkimsangjin <zlatkd13@gmail.com>
2023-04-19 20:52:30 +0900

    Monster 총알 발사 및 Player 사망

--- XR_Project_SJK/Assets/Script/PlayerHp.cs
+++ XR_Project_SJK/Assets/Script/PlayerHp.cs
@@ -1,12 +1,9 @@
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class PlayerHp : MonoBehaviour
 {
-    
     public int Hp = 5;
 
     public void Damaged(int Damage)
